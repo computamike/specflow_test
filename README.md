e5b8c88e700d08c09b8feace9b0aadac16db1924
Mike Hingley <mike.hingley@sainsburys.co.uk>
2024-01-29 00:34:10 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # specflow_test
 A test repository giving a simple example that can be used to test the issue raised in BlogPost https://support.specflow.org/hc/en-us/community/posts/4404727320593-All-Specflow-MSTests-running-in-the-background-after-single-test-run-when-started-from-Visual-Studio-Test-Explorer
+
+## How this code works 
+When the test executes, it writes a log into the test_explorer.log file, that will be located on your desktop.
+
+With Fine Code Coverage extension enabled - this seemed to cause the test to execute twice.
+
+```
+1/29/2024 12:29:13 AM Tst Add two numbers
+1/29/2024 12:29:22 AM Tst Add two numbers
+```
+
---
+++ SpecFlowTest/hooks.cs
@@ -0,0 +1,29 @@
+﻿using BoDi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowTest
+{
+    [Binding]
+    public class hooks
+    {
+        public ScenarioContext _scenarioContext { get; private set; } = null!;
+
+        public hooks(ScenarioContext scenarioContext, IObjectContainer container)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+            [BeforeScenario] // -> Notice how we're doing these steps before each scenario
+        public async Task RegisterSingleInstancePractitioner()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = Path.Combine(path, "test_explorer.log");
+            File.AppendAllText(filePath, $"{Environment.NewLine}{DateTime.Now} {_scenarioContext.ScenarioInfo.Title}");
+            await Console.Out.WriteLineAsync("STARTING TEST");
+        }
+    }
+   }
